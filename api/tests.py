13f59d82edae779e25ad2cd7a6ed8391039d8fb0
Mohammed Shurafa <shorafahamod@gmail.com>
2021-03-01 14:01:49 +0200

    Add pyton github unittest

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from unittest import TestCase
+
+
+# from django.test import TestCase
 
 # Create your tests here.
+
+
+def tow_sum(a, b):
+    return a + b
+
+
+class TestSum(TestCase):
+    def test_sun(self):
+        self.assertEqual(tow_sum(1, 2), 3)
